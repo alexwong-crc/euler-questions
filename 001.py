4e6f81cda3256955f18407c9fe7695c8dac2ed4d
Alex Wong <alex.wong@clientrelationship.com>
2019-11-13 00:37:46 +0000

    refactored solution to problem 001

--- 001.py
+++ 001.py
@@ -1,9 +1,16 @@
-maxNumber = 1000
-numberToAdd = []
-# multiples of 3 and 5
-# sum all those numbers
-for x in range(1, maxNumber):
-    if( x % 3 == 0 or x % 5 == 0):
-      numberToAdd.append(x)
+# Method 1
+# maxNumber = 1000
+# numberToAdd = []
+# for x in range(1, maxNumber):
+#     if( x % 3 == 0 or x % 5 == 0):
+#       numberToAdd.append(x)
 
-print(sum(numberToAdd))
+# print(sum(numberToAdd))
+
+# Method 2
+total = 0
+for n in range(1000):
+    if n % 3 == 0 or n % 5 == 0:
+        total = total + n
+
+print(total)
