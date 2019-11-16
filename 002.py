782258e0d3e85a34c1aebab4fcfc5cd59841a73a
Alex Wong <alex.wong@clientrelationship.com>
2019-11-16 13:29:18 +0000

    refactored first attempt and noted down a solution I saw online

--- 002.py
+++ 002.py
@@ -1,13 +1,50 @@
-fibonacci = [1]
-term = 2
-i = 0
-total = 0
+# First attempt
+def firstAttempt():
+    fibonacci = [1]
+    term = 2
+    i = 0
+    total = 0
 
-while term < 4000000:
-    if term % 2 == 0:
-        total += term
-    fibonacci.append(term)
-    term = fibonacci[i] + fibonacci[i + 1]
-    i += 1
+    while term < 4000000:
+        if term % 2 == 0:
+            total += term
+        fibonacci.append(term)
+        term = fibonacci[i] + fibonacci[i + 1]
+        i += 1
 
-print(total)
+    print("solution 1: ", total)
+
+
+# Second Attempt
+def secondAttempt():
+    sequence = [1, 2]
+    total = 0
+
+    while sequence[-1] < 4000000:
+        if sequence[-1] % 2 == 0:
+            total += sequence[-1]
+        newTerm = sequence[-1] + sequence[-2]
+        sequence.append(newTerm)
+
+    print("solution 2: ", total)
+
+
+# A posted answer I liked
+def postedAnswer():
+    firstTerm = 1
+    lastTerm = 2
+    total = 0
+
+    while lastTerm <= 4000000:
+        if lastTerm % 2 == 0:
+            total += lastTerm
+        oldLastTerm = lastTerm
+        lastTerm = lastTerm + firstTerm
+        firstTerm = oldLastTerm
+
+    print(total)
+
+
+# firstAttempt()
+secondAttempt()
+# postedAnswer()
